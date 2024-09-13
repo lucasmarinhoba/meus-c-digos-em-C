5586d0c372ff8c868a1ddaf81e6d42894fc7e167
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2024-09-13 10:31:13 -0300

    Update and rename regrasiniciais1.c to regras iniciais/exercicio1.c

--- regras iniciais/exercicio1.c
+++ regras iniciais/exercicio1.c
@@ -3,11 +3,11 @@ int main() {
     printf("Insira a temperatura em Celsius:\n");
     scanf("%f", &temperatura_c);
     temperatura_f = (temperatura_c*9/5) + 32;
-    printf("A temperatura em Fahrenheit é: %.2f\n",temperatura_f);
+    printf("A temperatura em Fahrenheit Ã©: %.2f\n",temperatura_f);
 
     printf("Insira a distancia em Kilometros:\n");
     scanf("%f", &distancia_km);
     distancia_milhas = distancia_km*0.621371;
-    printf("A distância em milhas é: %.2f\n",distancia_milhas);
+    printf("A distÃ¢ncia em milhas Ã©: %.2f\n",distancia_milhas);
     return 0;
 }
